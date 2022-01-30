7466a7ef9e843601d23f6ca10fc7aeb1b83400f5
kitayama3352 <kitayama3352@gmail.com>
2022-01-30 20:59:21 +0900

    sample

--- sample4.rb
+++ sample4.rb
@@ -1,5 +1,11 @@
 languages = ["日本語","英語","スペイン語"]
-puts languages
-puts  "----------"
-puts languages[1]
-puts "#{languages[0]}を話せます"
+
+  puts languages
+  puts  "----------"
+  puts languages[1]
+  puts "#{languages[0]}を話せます"
+  puts  "----------"
+
+languages.each do |language|
+  puts "#{language}を話せます"
+end
