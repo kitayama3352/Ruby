1435665140ef138abd76312423234ab1a6916ac6
kitayama3352 <kitayama3352@gmail.com>
2022-01-31 22:55:40 +0900

    sample

--- sample6.rb
+++ sample6.rb
@@ -1,3 +1,8 @@
 exam = {:subject => "Math" , :score => 80}
+border = "---------"
 puts exam[:subject]
 puts exam[:score]
+puts border
+
+exam = {subject: "Math" , score: 80}
+puts "#{exam[:subject]}: #{exam[:score]}点"
