876fe543526ec45a7d2fe0b8e35499fabcb9b9ca
kitayama3352 <kitayama3352@gmail.com>
2022-02-03 20:21:38 +0900

    sample

--- sample9.rb
+++ sample9.rb
@@ -16,6 +16,14 @@ characters = [
 ]
 
 characters.each do |character|
+
   puts "---------------------"
   puts "名前は#{character[:name]}です"
+
+  if character[:age]
+    puts "年齢は#{character[:age]}歳です"
+  else
+    puts "年齢は不明です"
+  end
+
 end
